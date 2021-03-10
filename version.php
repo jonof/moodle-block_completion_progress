b5536c15884fe5393b62f4187a310b1f710a2dcc
ggrinman <ggrinman@users.noreply.github.com>
2021-03-10 18:34:30 +0000

    GitHub Action increased the version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2020081001;
+$plugin->version = 2021031000;
 $plugin->requires  = 2018051700; // Moodle 3.5.0
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'Version for Moodle 3.5 onwards';
