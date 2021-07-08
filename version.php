fb7072c8b9d01879609d68a1feac8111d89b419e
Jonathon Fowler <fowlerj@usq.edu.au>
2021-07-09 09:43:12 +1000

    bump version for release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2021050400;
+$plugin->version   = 2021070900;
 $plugin->requires  = 2019111800;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'Version for Moodle 3.8 onwards';
