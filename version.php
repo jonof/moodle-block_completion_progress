249991a1491a4e1a97e5b25ee489d8e54325e505
Jonathon Fowler <fowlerj@usq.edu.au>
2022-02-01 08:37:10 +1000

    prepare a beta version of changes to date
    Resolves these issues:
      #67 Reimplement overview table based on flexible table
      #57 #76 Download overview table
      CONTRIB-8320 CONTRIB-8832 Add name filters
      CONTRIB-7928 Ampersand in module name

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2021070900;
+$plugin->version   = 2022020100;
 $plugin->requires  = 2020061500;
-$plugin->maturity  = MATURITY_STABLE;
+$plugin->maturity  = MATURITY_BETA;
 $plugin->release   = 'Version for Moodle 3.9 onwards';
 $plugin->component = 'block_completion_progress';
