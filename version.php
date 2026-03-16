be22bd9304a2c633ce66f7e1dc91f01371fe2dc7
Jonathon Fowler <fowlerj@usq.edu.au>
2026-03-17 09:56:17 +1000

    set supported Moodle version range
    fixes #125

--- version.php
+++ version.php
@@ -29,3 +29,4 @@ $plugin->requires  = 2022112800;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'Version for Moodle 4.5 onwards';
 $plugin->component = 'block_completion_progress';
+$plugin->supported = [405, 502];
